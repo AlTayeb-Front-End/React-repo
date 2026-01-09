d91071c21c0e38a8a0633e43b1769628a1be44ad
Eng-AlTayeb.K <altayibkhalid132001@gmail.com>
2026-01-09 09:31:23 +0200

    state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,70 @@
-import "./style/global.css";
-import TagButtons from "./sections/TagButtons";
-import Post from "./components/Post";
-import Header from "./sections/Header";
-import Posts from "./sections/Posts";
-import BasicForm from "./components/BasicForm";
+// import "./style/global.css";
+// import TagButtons from "./sections/TagButtons";
+// import Post from "./components/Post";
+// import Header from "./sections/Header";
+// import Posts from "./sections/Posts";
+// import BasicForm from "./components/BasicForm";
+// import ToDoList from "./sections/ToDo";
+
+// function App() {
+//   return (
+//     <>
+//       {/* <Header /> */}
+//       <ToDoList />
+//     </>
+//   );
+// }
+
+// export default App;
+
 import { useState } from "react";
 
-function App() {
-  const [formData, setFormData] = useState({
-    userName: "",
-    password: "",
-    email: "",
-    age: 0,
-    checkedBox: [],
-    radioBox: "",
-    country: "",
-  });
+const initialProducts = [
+  {
+    id: 110,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 111,
+    name: "Cheese",
+    count: 5,
+  },
+  {
+    id: 112,
+    name: "Spaghetti",
+    count: 2,
+  },
+];
+
+export default function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
+
+  function handleIncreaseClick(productId) {
+    console.log(productId);
+    let updatedProducts = products.map((product) => {
+      return product.id === productId
+        ? { ...product, count: product.count + 1 }
+        : product;
+    });
+    setProducts(() => updatedProducts);
+  }
+
   return (
-    <>
-      <Header />
-      <BasicForm
-        method={"GET"}
-        action={"/"}
-        onSubmit={(event) => {
-          event.preventDefault();
-          console.log(formData);
-        }}
-        formData={formData}
-        setData={setFormData}
-      />
-      <div>{formData.userName}</div>
-      <div>{formData.password}</div>
-      <div>{formData.email}</div>
-      <div>{formData.age}</div>
-      <div>{formData.checkedBox}</div>
-      <div>{String(formData.radioBox)}</div>
-      <div>{String(formData.country)}</div>
-    </>
+    <ul style={{ margin: "150px auto", width: "fit-content" }}>
+      {products.map((product) => (
+        <li key={product.id} style={{ marginBottom: "15px" }}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            style={{ marginLeft: "10px" }}
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
-
-export default App;
